a393e6b5b403db8404571b5795948e33ba08a3d8
iplace <iplace@126.com>
2017-04-20 17:03:00 +0800

    删除todo的model

--- todolist/urls.py
+++ todolist/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url
-
 from . import views
-
 
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^todolist/$', views.todo_list),
+    url(r'^delete/(?P<id>[0-9]+)/$', views.delete_todo),
 ]
--- todolist/views.py
+++ todolist/views.py
@@ -1,5 +1,6 @@
+# coding:utf-8
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
@@ -12,16 +13,6 @@ def index(request):
     return render(request, 'todolist/index.html', {'title': 'Todo List'})
 
 
-class JSONResponse(HttpResponse):
-    '''
-    用来返回JSON数据
-    '''
-    def __init__(self, data, **kwargs):
-        content = JSONRenderer.render(data)
-        kwargs['content_type'] = 'application/json'
-        super(JSONResponse, self).__init__(content, **kwargs)
-
-
 @csrf_exempt
 def todo_list(request):
     '''
@@ -30,12 +21,27 @@ def todo_list(request):
     if request.method == 'GET':
         todos = Todolist.objects.all()
         serializer = TodolistSerializer(todos, many=True)
-        return JSONResponse(serializer.data)
+        return JsonResponse(serializer.data, safe=False)
 
     elif request.method == 'POST':
-        todo = JSONParser().parse(request)
+        data = JSONParser().parse(request)
         serializer = TodolistSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
-            return JSONParser(serializer.data, status=201)
-        return JSONParser(serializer.errors, status=400)
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+
+@csrf_exempt
+def delete_todo(request, id):
+    '''
+    删除对应todo
+    '''
+    try:
+        todo = Todolist.objects.get(id=id)
+    except Todolist.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'DELETE':
+        todo.delete()
+        return HttpResponse(status=204)
